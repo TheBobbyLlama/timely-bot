9b4b1094239a9cec8c23f081b23789ec1957e571
Gwen <akamitsunami@gmail.com>
2022-04-15 13:15:07 -0500

    Put more in .env

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ client.once('ready', () => {
 			auth_uri: "https://accounts.google.com/o/oauth2/auth",
 			token_uri: "https://oauth2.googleapis.com/token",
 			auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-			client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-pln7w%40timekeeper-bot.iam.gserviceaccount.com"
+			client_x509_cert_url: process.env.SA_CLIENT_CERT_URL
 		}),
-		databaseURL:"https://timekeeper-bot-default-rtdb.firebaseio.com" 
+		databaseURL: process.env.SA_DATABASE_URL
 	});
 
 	database = firebaseAdmin.database();
